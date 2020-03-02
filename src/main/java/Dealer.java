048811d73d74ac5b3d8a26e7e75fafc74f839853
Ronaldo Miguel <miguel.ronaldo1809@gmail.com>
2020-03-01 21:27:01 -0600

    Dealer class done

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -6,6 +6,15 @@ public class Dealer {
     private Deck theDeck;
     private ArrayList<Card> dealersHand;
 
+
+    public Deck getTheDeck() {
+        return theDeck;
+    }
+
+    public ArrayList<Card> getDealersHand() {
+        return dealersHand;
+    }
+
     //sets up a brand new deck to start off
     public Dealer() {
         theDeck = new Deck();
@@ -13,7 +22,13 @@ public class Dealer {
 
     //takes 3 cards from the deck for dealer to use
     public ArrayList<Card> dealHand(){
-        
+        if(theDeck.size() <= 34){
+            theDeck.newDeck();
+        }
+        dealersHand.add(theDeck.remove(0));
+        dealersHand.add(theDeck.remove(1));
+        dealersHand.add(theDeck.remove(2));
+        return dealersHand;
     }
 
 
