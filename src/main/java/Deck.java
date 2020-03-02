04cf20f5d79f7b9cabbb7442fc2ef6ede2c74196
Ronaldo Miguel <miguel.ronaldo1809@gmail.com>
2020-03-01 22:13:55 -0600

    Deck class fix

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -3,6 +3,8 @@ import java.util.Collections;
 
 public class Deck extends ArrayList<Card> {
 
+    private ArrayList<Card> deck = new ArrayList<Card>();
+
     //adds Cards class into the deck class then shuffles for a randomize order
     public Deck() {
         for(int i = 2; i < 15; i++){
@@ -17,6 +19,7 @@ public class Deck extends ArrayList<Card> {
     //clears the current deck and replaces it with a brand new shuffle deck
     public void newDeck() {
         this.clear();
+        deck = new Deck();
     }
 
 }
