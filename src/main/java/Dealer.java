afd935e546203a195b0d0bb390095f16ab8ae3ed
Ronaldo Miguel <miguel.ronaldo1809@gmail.com>
2020-03-01 20:52:39 -0600

    Fixed Deck class and added Dealer class

---
+++ src/main/java/Dealer.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Dealer {
+    //set the data members
+    private Deck theDeck;
+    private ArrayList<Card> dealersHand;
+
+    public Dealer(){
+
+    }
+}
--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -3,22 +3,21 @@ import java.util.Collections;
 
 public class Deck extends ArrayList<Card> {
 
-    ArrayList<Card> deck = new ArrayList<Card>();
-
     //adds Cards class into the deck class then shuffles for a randomize order
     public Deck(){
         for(int i = 2; i < 15; i++){
-            deck.add(new Card('C', i));
-            deck.add(new Card('D', i));
-            deck.add(new Card('H', i));
-            deck.add(new Card('S', i));
-            Collections.shuffle(deck);
+            this.add(new Card('C', i));
+            this.add(new Card('D', i));
+            this.add(new Card('H', i));
+            this.add(new Card('S', i));
+            Collections.shuffle(this);
         }
     }
 
     //clears the current deck and replaces it with a brand new shuffle deck
     public ArrayList<Card> newDeck(){
-        deck.clear();
+        this.clear();
         return new Deck();
     }
+
 }
